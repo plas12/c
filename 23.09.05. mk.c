4f9a1204b2cd6dfddb81de2a03b20837b323415f
plas12 <143487328+plas12@users.noreply.github.com>
2023-09-05 17:46:59 +0900

    Add files via upload

---
+++ 23.09.05. mk.c
@@ -0,0 +1,48 @@
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+ int = 자료형 설정 
+ return = 자료형 일때 끝냄  
+ char =
+ double
+ float
+ for
+ if
+ case
+ switch
+ else
+ const
+ .
+ .
+ .
+ 
+ 
+ % = 나누고 난 나머지 
+ & = 그리고 
+ | = 또는 
+ 
+ \ = 달러 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+int main(int argc, char *argv[]) {
+
+printf ("흐하ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ");
+
+
+	return 0;
+}
+*/
