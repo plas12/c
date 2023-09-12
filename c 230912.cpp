2a0743d888621888f42d22a1c4a72d6922a57d11
plas12 <143487328+plas12@users.noreply.github.com>
2023-09-12 17:52:35 +0900

    Add files via upload

---
+++ c 230912.cpp
@@ -0,0 +1,154 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	printf("오늘은 %d 월 %d 일 입니다\n", 9,12);
+	printf("지금은 %d시 %d분 입니다 \n", 5,39);
+	printf("밑변이 3cm, 높이가 4cm인 삼각형의 넓이는 %d 입니다 \n", 6);
+	printf("12378m는 %.3lfkm 입니다\n",12.378) ;
+	printf("34.536kg은 %dg 입니다\n", 34536);
+	printf("34.5%는 %d할 %d푼 %d리 입니다\n", 3,4,5 ) ;
+	return 0 ;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*int main(){
+	printf("%d,%d\n",153,-153);
+	printf("%.3lf,%.3lf\n", 42.195,-42.195);
+	printf("%c, %c\n",'A','a');
+	printf("%s,%s\n", "Artlflcial lntelligence","마빈 민스키");
+	printf("%d,%o,%x\n",255,255,255) ;
+	
+	
+	
+	
+	return 0 ;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(){
+	printf("%d년 %d월 알파고 승리\n",2019,3)	;
+	printf("%dD 프린터로 집 짓기\n",3) ;
+	printf("원주율 = %.2lf\n",3.14);
+	printf("%.3lfkm 마라톤",42.195);
+	
+	
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(){
+	int grade = 5;
+	int Class = 2;
+	int number = 1;
+	
+	printf ("%d학년 %d반 %d번 ", grade, Class, number) ;
+
+	
+}
+
+int main (){
+printf("나의이름은%s 입니다.\n","홍길동"); 문자열	
+printf("나는%d살 입니다\n", 2*10); 정수 
+printf("나의 혈액형은 %c입니다\n",'b'); 문자 
+printf("나의 시력은 %.2lf / %.2lf 입니다\n",2.0,2.0)	; 실	
+	return 0;
+}
+%lf에서 %.[숫자]lf 로 바꾸면 소수점이 숫자만큼 나온다.
+
+
+	중요!! 
+	int main(){ 
+	
+	return 0 ;
+	}
+	
+	
+	%d는 정수인 변수 호출에 쓴다 
+	이때 사용 순서는 왼쪽에서 순서대로 쓴다 
+*/ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
