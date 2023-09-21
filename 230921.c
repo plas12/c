8777ae706823986d206ab5310d55a3fbd1aa897d
plas12 <143487328+plas12@users.noreply.github.com>
2023-09-21 17:06:34 +0900

    Add files via upload

---
+++ 230921.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main (){
+	int m;
+	int cm;
+	
+	
+	printf("m¸¦ ÀÔ·Â ");
+	scanf("%d", &m);
+	cm = m * 100;
+	printf(" %dcm ",cm);
+	
+	
+	
+	return 0;
+}
+	
+
