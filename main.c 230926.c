3bd977c4f915f2faa76fbe35630a05b2235822d5
plas12 <143487328+plas12@users.noreply.github.com>
2023-09-26 17:49:40 +0900

    Add files via upload

---
+++ main.c 230926.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	double a = 0;
+	double b = 0;
+	 
+	scanf("%lf", &a);
+	b = (a* 9.0/5.0) + 32;
+	printf("%lf",b);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*printf("%d\n", 10/3);
+	printf("%d\n", 10/3.0);
+	printf("%lf\n", 10/3.0);
+	int a = 0;
+	int n1 = 0;
+	int n2 = 0;
+	
+	scanf("%d  %d", &n1, &n2);
+	
+	a = n1 + n2;
+	printf("[+]%d\n", a);
+	
+	a = n1 - n2;
+	printf("[-]%d\n", a);
+	
+	a = n1 * n2;
+	printf("[*]%d\n", a);
+	
+	a = n1 / n2;
+	printf("[/]%d\n", a);
+	
+	a = n1 % n2;
+	printf("[%%]%d\n", a);
+	
+	
+	
+	정수형 연산의 결과는 정수
+	실수형 연산의 결과는 실수
+	정수 [사칙 계산] 정수 = 정수
+	정수 [사칙 계산] 실수 = 실수 
+	*/
+	return 0;
+}
