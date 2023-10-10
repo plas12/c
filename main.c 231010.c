b7b22d9759d6e55f9ffd59787448610b6889b774
plas12 <143487328+plas12@users.noreply.github.com>
2023-10-10 17:55:12 +0900

    Add files via upload

---
+++ main.c 231010.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+	/*printf("%d \n", 2*5000>15*1000);
+	printf("%d \n", (1000/2) - (1000/3) > 100 );
+	printf("%d \n", 500*5 + 1000*2 <= 5000);
+	printf("%d", 15000 - 5000*1 >= 2000*4 || 15000-5000*2 >=2000*3);
+	
+	
+	
+	
+	
+	
+	int a;
+		
+	scanf("%d", &a);
+	
+	printf("%d\n", a/7 == 5 );
+	printf("%d\n", a%3==0);
+	printf("%d\n", a%5!=0);
+	printf("%d\n", (a%3==0 && a%2 == 0));
+	printf("%d", (a%4==0 ||a%7==0));
+	
+	int n;
+	
+	
+	scanf("%d", &n);
+	printf("%d\n", n%2==0);
+	printf("%d\n", n%3==0);
+	printf("%d\n", n%2==1);
+	printf("%d\n", n%3!=0 );
+	
+	printf("%d", !1);
+	
+	int res;
+	
+	res = 0&&0;
+	printf("%d\n", res);
+	res = 0&&1;
+	printf("%d\n", res);
+	res = 1&&0;
+	printf("%d\n", res);
+	res = 1&&1;
+	printf("%d\n", res);
+	
+	res = 0||0;
+	printf("%d\n", res);
+	res = 0||1;
+	printf("%d\n", res);
+	res = 1||0;
+	printf("%d\n", res);
+	res = 1||1;
+	printf("%d\n", res);
+	
+	printf("%d\n", !res);
+	
+	printf("%d\n", 10>5&&8<3);
+	printf("%d\n", 10>5||8<3);
+	printf("%d\n", !(10>8));
+	
+	
+	
+	
+	int a;
+	int b;
+	
+	a = 23;
+	b = 24;
+	
+	printf("%d\n", a>b);
+	
+	printf("%d\n", a<b);
+	
+	
+	
+	a&&b a그리고b 
+	a||b a 또는b 
+	!a  a가 아니다 
+	*/
+	return 0;
+	
+}
