7d26c5d3713c3d8be7fd15b8359cda89d68686f4
plas12 <143487328+plas12@users.noreply.github.com>
2023-10-31 17:50:48 +0900

    Add files via upload

---
+++ main.cpp231031.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+
+
+
+int main(int argc, char** argv) {
+	/*
+	int a, i;
+	
+	scanf("%d", &a);
+	
+	for(i = 1; i <= a; i++){
+		if(i % 3 == 0){
+			printf("%d ", i);
+		}
+	}
+	
+	
+	int a, b, i;
+	
+	scanf("%d", &a);
+	b = 0;
+	for(i = 1; i <= a; i++){
+		b = b + i;
+	}
+	printf("%d", b);
+	
+	
+	int i;
+	int a, b, c;
+	a = -1;
+		
+	for(i = 0; i < 10; i++){
+		a = a * -1;
+		printf("%d ", a);
+		
+	}
+	printf("\n");
+	for(i = 0; i < 10; i++){
+		b = i + 1;
+		printf("%d ", b % 2);
+		
+	}
+	printf("\n");
+	for(i = 0; i < 10; i++){
+		c = i + 1;
+		a = a * -1;
+		printf("%d ", c * a);
+		
+	}
+	printf("\n");
+	
+	
+	for(int i = 0; i < 10; i++){
+		printf("i = %d\n", i);
+	}
+	
+	printf("마지막 i = %d\n", i);
+	
+	
+	int i;
+	
+	for(i = 0; i < 10; i++){
+		printf("i = %d\n", i);
+	}
+	
+	printf("마지막 i = %d\n", i);
+	
+	
+	int n;
+	
+	for(n = -10; n <= -1; n++){
+		printf("n = %d\n", n);
+	}
+	print("마지막 n = %d\n", n);
+	*/
+	return 0;
+}
