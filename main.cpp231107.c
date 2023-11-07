10002d08d36c72221b8a32230463be24426f532e
plas12 <143487328+plas12@users.noreply.github.com>
2023-11-07 17:51:56 +0900

    Add files via upload

---
+++ main.cpp231107.c
@@ -0,0 +1,173 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(int argc, char *argv[]) {
+	int i = 0;
+	int a = 0;
+	
+	scanf("%d", &a);
+	
+	while(){
+		12345 % 10   12345/10 % 10   12345/10/10% 10
+	}
+	
+	/*
+	
+	
+	
+	
+	
+	int i = 0;
+	int a = 0;
+	int b = 1;
+	
+	scanf("%d", &a);
+	
+	while(i <= a){
+		b = b * -1;
+		printf("%d ", b);
+		i++;
+	} 
+	printf("\n");
+	
+	i = 0;
+	a = 0;
+	b = 1;
+	
+	scanf("%d", &a);
+	
+	while(i <= a){
+		i++;
+		printf("%d", i % 2);
+	}
+	printf("\n");
+	
+	i = 0;
+	a = 0;
+	b = -1;
+	
+	
+	
+	scanf("%d", &a);
+	
+	while(i <= a){
+		i++;
+		b = b * -1;
+		printf("%d ", i * b);
+	}
+	
+	
+	int i = 0;
+	
+	while(i < 10){
+		if(i == 5){
+			i++;
+			continue; 	-	continue는  continue아래의 실행문들을 무시한다
+		}
+		printf("i의 값 : %d\n", i);
+		i++;
+	}
+
+	
+
+	int i = 0;
+	
+	while(i < 10){
+		printf("i의 값 : %d\n", i);
+		i++;
+		if(i == 5){
+			break; 
+		}
+	} 
+	
+	
+	int i = 11;
+	
+	while(i <= 10){
+		printf("%d ", i);
+		i++;
+	}
+	
+	i = 11;
+	
+	printf("\n");
+	
+	do{    -    반복문 안의 실행문을 실행 하고 while판단을 뒤에서 한다
+		printf("%d ", i);
+		i++;
+	}while(i <= 10);
+	
+	
+	int n = 10;
+	int num = 0;
+	int bun = 0;
+	
+	while(n<=17){
+		printf("%d ", n);
+		num = num + n;
+		bun++;
+		n++;
+	}
+	printf("\nwhile문 종료후 n의 값 : %d\n", n);
+	printf("\n num : %d bun : %d\n", num, bun);	
+	
+	n = 20;
+	num = 0;
+	bun = 0;
+	
+	while(n>=15){
+		printf("%d ", n);
+		num = num + n;
+		bun++;
+		n--;
+	}
+	printf("\nwhile문 종료후 n의 값 : %d\n", n);
+	printf("\n num : %d bun : %d", num, bun);
+	
+	
+		int n = 10;
+	
+	while(n<=17){
+		n++;
+		printf("%d ", n);
+	}
+	printf("\nwhile문 종료후 n의 값 : %d\n", n);
+	
+	n = 20;
+	
+	while(n>=15){
+		n--;
+		printf("%d ", n);
+	}
+	printf("\nwhile문 종료후 n의 값 : %d\n", n);
+	
+	
+	int n = 10;
+	
+	while(n<=17){
+		printf("%d ", n);
+		n++;
+	}
+	printf("\nwhile문 종료후 n의 값 : %d\n", n);
+	
+	n = 20;
+	
+	while(n>=15){
+		printf("%d ", n);
+		n--;
+	}
+	printf("\nwhile문 종료후 n의 값 : %d\n", n);
+	
+	
+	int i;
+	i = 1;
+	
+	while(i<=10){    -    ()안의 식이 성립할때까지 반복 
+		printf("%d", i);
+		i++;
+	}
+	*/
+	return 0;
+}
