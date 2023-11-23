bc323c9d59f2f4c5205a0e7989cf4d37ec94cdaf
plas12 <143487328+plas12@users.noreply.github.com>
2023-11-23 16:57:53 +0900

    Add files via upload

---
+++ main.c231123.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(int argc, char *argv[]) {
+	/*
+	int ia = 0;
+	int ib = 0;
+	int ic = 0;
+	int a = 6;
+	
+	for(ia = 5; ia >= 1; ia--){
+		for(ib = 0; ib >= 1 - ia; ib--){
+			printf(" ");
+		}
+		for(ic = ia; ic <= 5; ic++){
+			printf("%d", ic);
+		}
+		printf("\n");
+	}
+	
+	
+	int ia = 0;
+	int ib = 0;
+	int ic = 0;
+	int id = 0;
+	int a = 0;
+	
+	for(ia = 1; ia <= 5; ia++){
+		for(ib = 1; ib <= 6 - ia;  ib++){
+			printf(" ");
+		}
+		for(ic = 1; ic <= ia; ic++){
+			printf("%d", ic);
+		}
+		a++;
+		for(id = a-1; id >= 1; id--){
+			printf("%d", id);
+		}
+		printf("\n");
+	}
+	
+	
+	int i = 0;
+	int ia = 0;
+	int ib = 0;
+	int a = 0;
+	int b = 0;
+	
+	for(i = 1; i <= 100; i++){
+		a = a + i;
+	}
+	for(ia = 1; ia <=100; ia++){
+		for(ib = 1; ib <= ia; ib++){
+			if(ia % ib == 0){
+				b++;
+			}
+		}
+		if(b == 2){
+			a = a - ia;
+		}
+		b = 0;
+	}
+	printf("%d", a);
+	
+	
+	int i = 0;
+	int ia = 0;
+	int a = 0;
+	
+	for(i = 100; i <= 999; i++){
+		for(ia = 1; ia <= i; ia++){
+			if(i % ia == 0){
+				a++;	
+			}
+		}
+		if(a == 2){
+			printf("%3d\n", i);
+		}
+		a = 0;
+	}
+*/
+	return 0;
+}
