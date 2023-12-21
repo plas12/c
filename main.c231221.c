789829313f3d566c3ff0dc0c343b1070d8d45f82
plas12 <143487328+plas12@users.noreply.github.com>
2023-12-21 17:03:39 +0900

    Add files via upload

---
+++ main.c231221.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main(int argc, char *argv[]) {
+	
+	
+	/*
+	int ar[6][6] = {0};
+	int i, j;
+	int b = 0;
+	
+	srand(time(NULL));
+	
+	for(i = 0; i < 6; i++){
+		for(j = 0; j < 7; j++){
+			ar[i][j] = rand() % 100 + 1;
+			if(b<ar[i][j]){
+				b = ar[i][j];
+			}
+		}
+	}
+	
+	i  = 0;
+	while(i < 6){
+		j =0;
+		while(j < 7){
+			printf("%3d", ar[i][j]);
+			j++;
+		}
+		printf("\n");
+		i++;
+	}
+	
+	printf("%d", b);
+	
+	int a = 0;
+	
+	for(i = 0; i < 6; i++){
+		for(j = 0; j < 6; j++){
+			a = a + ar[i][j];
+		}
+	}
+	printf("\n\n%d", a);
+	
+	
+	
+	
+	int arr[6][5] = {{23, 22, 27, 21, 24},
+	{21, 22, 25, 24, 23},
+	{20, 23, 22, 21, 24},
+	{25, 26, 24, 27, 28},
+	{26, 24, 28, 29, 26},
+	{27, 28, 29, 26, 27}};
+				 
+	int i;
+	int j;
+	
+	for( i = 0; i < 6; i++){
+		for( j  = 0; j < 5; j++){
+			printf("%dÇÐ³â %d¹Ý : %d \t", i + 1, j + 1, arr[i][j]);
+		}
+		printf("\n");
+	}
+	*/
+	return 0;
+}
