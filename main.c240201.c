c1d3141804960cf65996b395f10279af9d2506a4
plas12 <143487328+plas12@users.noreply.github.com>
2024-02-01 17:01:22 +0900

    Add files via upload

---
+++ main.c240201.c
@@ -0,0 +1,163 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+	/*
+	9
+	char ch;
+	
+	while(1){
+		scanf("%c", &ch);
+		if(ch == 10){
+			break;
+		}
+		if(65<=ch&&ch<=90){
+			printf("%c", ch);
+		}
+		else{
+		}
+		if(97<=ch&&ch<=122){
+			printf("%c", ch-32);
+		}
+		else{
+		}
+	}
+	
+	
+	8
+	char ch;
+	
+	while(1){
+		scanf("%c", &ch);
+		if(ch == 10){
+			break;
+		}
+		if(65<=ch&&ch<=90){
+			printf("%c", ch);
+		}
+		else{
+		}
+	}
+	
+	
+	7
+	char ch;
+	
+	while(1){
+		scanf("%c", &ch);
+		if(ch == 10){
+			break;
+		}
+		if(48<=ch&&ch<=57){
+			printf("%c", ch);
+		}
+		else{
+		}
+	}
+	
+	
+	6
+	char ch;
+	
+	while(1){
+		scanf("%c", &ch);
+		if(ch == 10){
+			break;
+		}
+		if(65<=ch&&ch<=90){
+			printf("%c", ch);
+		}
+		else{
+		}
+		if(97<=ch&&ch<=122){
+			printf("%c", ch);
+		}
+		else{
+		}
+	}
+	
+	
+	5
+	char ch;
+	
+	while(1){
+		scanf("%c", &ch);
+		if(ch == '\n'){
+			break;
+		}
+		printf("%c", ch);
+	}
+	
+	
+	4
+	char a;
+	
+	while(1){
+		scanf("%c", &a);
+		printf("%d", a);
+		if(a == '\n'){
+			break;
+		}
+	}
+
+	
+	3
+	double a;
+	char b;
+	char c;
+	
+	scanf("%lf", &a);
+	scanf("%c", &b);
+	scanf("%c", &c);
+	 
+	printf("\n%d  %d", b, c);
+	
+	
+	2
+	int a;
+	char b;
+	
+	scanf("%d", &a);
+	scanf("%c", &b);
+	
+	printf("%d", b);
+	
+	
+	1
+	int a1[10] = {};
+	int b2[10] = {};
+	int winnerisa = 0;
+	int winnerisb = 0;
+	int i;
+	
+	printf("a : ");
+	for(i = 0; i < 10; i++){
+	 	scanf("%d", &a1[i]);
+	}
+	printf("b : ");
+	for(i = 0; i < 10; i++){
+		scanf("%d", &b2[i]);
+	}
+	
+	for(i = 0; i < 10; i++){
+		if(a1[i] > b2[i]){
+			winnerisa++;
+		}
+		if(a1[i] < b2[i]){
+			winnerisb++;
+		}
+	}
+	
+	if(winnerisa < winnerisb){
+		printf("b");
+	}
+	if(winnerisa > winnerisb){
+		printf("a");
+	}
+	if(winnerisa == winnerisb){
+		printf("---");
+	}
+	*/
+	return 0;
+}
